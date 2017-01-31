61d28e50e403306e312213d432a2325281c3988b
Abubakr Magzoub <a.magzoub@hwcomms.com>
2017-01-31 16:55:21 +0000

    add lists resources

--- app/src/main/java/com/devsuda/abubakr/ma7anak/ExpandableListDataPump.java
+++ app/src/main/java/com/devsuda/abubakr/ma7anak/ExpandableListDataPump.java
@@ -3,8 +3,8 @@ package com.devsuda.abubakr.ma7anak;
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
-import java.text.Collator;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -23,20 +23,8 @@ public class ExpandableListDataPump {
     }
 
     public static HashMap<String, List<String>> getData() {
-        HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
 
-        String[] processesValues = context.getResources().getStringArray(R.array.processes);
-
-        ArrayAdapter<String> processesAdaptor = new ArrayAdapter<String>(context,
-                R.layout.places_list_item, R.id.placeNameTv, processesValues);
-
-        final Collator col = Collator.getInstance();
-//        processesAdaptor.sort(new Comparator<ApplicationInfo>() {
-//            @Override
-//            public int compare(ApplicationInfo lhs, ApplicationInfo rhs) {
-//                return col.compare(lhs.loadLabel(packageManager),rhs.loadLabel(packageManager));
-//            }
-//        });
+        String[] placesValues;
 
         Comparator<String> ALPHABETICAL_ORDER1 = new Comparator<String>() {
             public int compare(String object1, String object2) {
@@ -45,162 +33,120 @@ public class ExpandableListDataPump {
             }
         };
 
-        // -----------
 
-
-        String[] placesValues = context.getResources().getStringArray(R.array.places);
-
-        List<String> locationsList = new ArrayList<String>();
+        placesValues = context.getResources().getStringArray(R.array.places);
         ArrayAdapter<String> locationsAdaptor = new ArrayAdapter<String>(context,
                 R.layout.places_list_item, R.id.placeNameTv, placesValues);
-
-        String placesValuesString = null;
+        List<String> locationsList1 = new ArrayList<String>();
         for (int i = 0; i < locationsAdaptor.getCount(); i++) {
-            placesValuesString = (String) locationsAdaptor.getItem(i);
-            locationsList.add(placesValuesString);
-            Collections.sort(locationsList,ALPHABETICAL_ORDER1);
+            locationsList1.add((String) locationsAdaptor.getItem(i));
+            Collections.sort(locationsList1, ALPHABETICAL_ORDER1);
         }
-
-
 
         // -----------
 
-        String[] placesValues2 = context.getResources().getStringArray(R.array.places2);
-
-        List<String> locationsList2 = new ArrayList<String>();
+        placesValues = context.getResources().getStringArray(R.array.places2);
         ArrayAdapter<String> locationsAdaptor2 = new ArrayAdapter<String>(context,
-                R.layout.places_list_item, R.id.placeNameTv, placesValues2);
-
-        String placesValuesString2 = null;
+                R.layout.places_list_item, R.id.placeNameTv, placesValues);
+        List<String> locationsList2 = new ArrayList<String>();
         for (int i = 0; i < locationsAdaptor2.getCount(); i++) {
-            placesValuesString = (String) locationsAdaptor2.getItem(i);
-            locationsList2.add(placesValuesString);
-            Collections.sort(locationsList,ALPHABETICAL_ORDER1);
+            locationsList2.add((String) locationsAdaptor2.getItem(i));
+            Collections.sort(locationsList2, ALPHABETICAL_ORDER1);
         }
 
         // -----------
 
-        String[] placesValues3 = context.getResources().getStringArray(R.array.places3);
-
-        List<String> locationsList3 = new ArrayList<String>();
+        placesValues = context.getResources().getStringArray(R.array.places3);
         ArrayAdapter<String> locationsAdaptor3 = new ArrayAdapter<String>(context,
-                R.layout.places_list_item, R.id.placeNameTv, placesValues3);
-
+                R.layout.places_list_item, R.id.placeNameTv, placesValues);
+        List<String> locationsList3 = new ArrayList<String>();
         for (int i = 0; i < locationsAdaptor3.getCount(); i++) {
-            placesValuesString = (String) locationsAdaptor3.getItem(i);
-            locationsList3.add(placesValuesString);
-            Collections.sort(locationsList,ALPHABETICAL_ORDER1);
+            locationsList3.add((String) locationsAdaptor3.getItem(i));
+            Collections.sort(locationsList3, ALPHABETICAL_ORDER1);
         }
 
         // -----------
 
-        String[] placesValues4 = context.getResources().getStringArray(R.array.places4);
-
-        List<String> locationsList4 = new ArrayList<String>();
+        placesValues = context.getResources().getStringArray(R.array.places4);
         ArrayAdapter<String> locationsAdaptor4 = new ArrayAdapter<String>(context,
-                R.layout.places_list_item, R.id.placeNameTv, placesValues4);
-
-        String placesValuesString4 = null;
+                R.layout.places_list_item, R.id.placeNameTv, placesValues);
+        List<String> locationsList4 = new ArrayList<String>();
         for (int i = 0; i < locationsAdaptor4.getCount(); i++) {
-            placesValuesString = (String) locationsAdaptor4.getItem(i);
-            locationsList4.add(placesValuesString);
-            Collections.sort(locationsList,ALPHABETICAL_ORDER1);
+            locationsList4.add((String) locationsAdaptor4.getItem(i));
+            Collections.sort(locationsList4, ALPHABETICAL_ORDER1);
         }
 
         // -----------
 
-        String[] placesValues5 = context.getResources().getStringArray(R.array.places5);
-
-        List<String> locationsList5 = new ArrayList<String>();
+        placesValues = context.getResources().getStringArray(R.array.places5);
         ArrayAdapter<String> locationsAdaptor5 = new ArrayAdapter<String>(context,
-                R.layout.places_list_item, R.id.placeNameTv, placesValues5);
-
-        String placesValuesString5 = null;
+                R.layout.places_list_item, R.id.placeNameTv, placesValues);
+        List<String> locationsList5 = new ArrayList<String>();
         for (int i = 0; i < locationsAdaptor2.getCount(); i++) {
-            placesValuesString = (String) locationsAdaptor5.getItem(i);
-            locationsList5.add(placesValuesString);
-            Collections.sort(locationsList,ALPHABETICAL_ORDER1);
+            locationsList5.add((String) locationsAdaptor5.getItem(i));
+            Collections.sort(locationsList5, ALPHABETICAL_ORDER1);
         }
 
         // -----------
 
-        String[] placesValues6 = context.getResources().getStringArray(R.array.places6);
-
-        List<String> locationsList6 = new ArrayList<String>();
+        placesValues = context.getResources().getStringArray(R.array.places6);
         ArrayAdapter<String> locationsAdaptor6 = new ArrayAdapter<String>(context,
-                R.layout.places_list_item, R.id.placeNameTv, placesValues6);
-
-        String placesValuesString6 = null;
+                R.layout.places_list_item, R.id.placeNameTv, placesValues);
+        List<String> locationsList6 = new ArrayList<String>();
         for (int i = 0; i < locationsAdaptor2.getCount(); i++) {
-            placesValuesString = (String) locationsAdaptor6.getItem(i);
-            locationsList6.add(placesValuesString);
-            Collections.sort(locationsList,ALPHABETICAL_ORDER1);
+            locationsList6.add((String) locationsAdaptor6.getItem(i));
+            Collections.sort(locationsList6, ALPHABETICAL_ORDER1);
         }
 
         // -----------
 
-        String[] placesValues7 = context.getResources().getStringArray(R.array.places7);
-
-        List<String> locationsList7 = new ArrayList<String>();
+        placesValues = context.getResources().getStringArray(R.array.places7);
         ArrayAdapter<String> locationsAdaptor7 = new ArrayAdapter<String>(context,
-                R.layout.places_list_item, R.id.placeNameTv, placesValues7);
-
-        String placesValuesString7 = null;
+                R.layout.places_list_item, R.id.placeNameTv, placesValues);
+        List<String> locationsList7 = new ArrayList<String>();
         for (int i = 0; i < locationsAdaptor7.getCount(); i++) {
-            placesValuesString = (String) locationsAdaptor7.getItem(i);
-            locationsList7.add(placesValuesString);
-            Collections.sort(locationsList,ALPHABETICAL_ORDER1);
+            locationsList7.add((String) locationsAdaptor7.getItem(i));
+            Collections.sort(locationsList7, ALPHABETICAL_ORDER1);
         }
 
         // -----------
 
-        String[] placesValues8 = context.getResources().getStringArray(R.array.places8);
-
-        List<String> locationsList8 = new ArrayList<String>();
+        placesValues = context.getResources().getStringArray(R.array.places8);
         ArrayAdapter<String> locationsAdaptor8 = new ArrayAdapter<String>(context,
-                R.layout.places_list_item, R.id.placeNameTv, placesValues8);
-
-        String placesValuesString8 = null;
+                R.layout.places_list_item, R.id.placeNameTv, placesValues);
+        List<String> locationsList8 = new ArrayList<String>();
         for (int i = 0; i < locationsAdaptor8.getCount(); i++) {
-            placesValuesString = (String) locationsAdaptor8.getItem(i);
-            locationsList8.add(placesValuesString);
-            Collections.sort(locationsList,ALPHABETICAL_ORDER1);
+            locationsList8.add((String) locationsAdaptor8.getItem(i));
+            Collections.sort(locationsList8, ALPHABETICAL_ORDER1);
         }
 
         // -----------
 
-        String[] placesValues9 = context.getResources().getStringArray(R.array.places9);
-
-        List<String> locationsList9 = new ArrayList<String>();
+        placesValues = context.getResources().getStringArray(R.array.places9);
         ArrayAdapter<String> locationsAdaptor9 = new ArrayAdapter<String>(context,
-                R.layout.places_list_item, R.id.placeNameTv, placesValues9);
-
-        String placesValuesString9 = null;
+                R.layout.places_list_item, R.id.placeNameTv, placesValues);
+        List<String> locationsList9 = new ArrayList<String>();
         for (int i = 0; i < locationsAdaptor9.getCount(); i++) {
-            placesValuesString = (String) locationsAdaptor9.getItem(i);
-            locationsList9.add(placesValuesString);
-            Collections.sort(locationsList,ALPHABETICAL_ORDER1);
+            locationsList9.add((String) locationsAdaptor9.getItem(i));
+            Collections.sort(locationsList9, ALPHABETICAL_ORDER1);
         }
 
         // -----------
 
-        String[] placesValues10 = context.getResources().getStringArray(R.array.places10);
-
-        List<String> locationsList10 = new ArrayList<String>();
+        placesValues = context.getResources().getStringArray(R.array.places10);
         ArrayAdapter<String> locationsAdaptor10 = new ArrayAdapter<String>(context,
-                R.layout.places_list_item, R.id.placeNameTv, placesValues10);
-
+                R.layout.places_list_item, R.id.placeNameTv, placesValues);
+        List<String> locationsList10 = new ArrayList<String>();
         String placesValuesString10 = null;
         for (int i = 0; i < locationsAdaptor10.getCount(); i++) {
-            placesValuesString = (String) locationsAdaptor10.getItem(i);
-            locationsList10.add(placesValuesString);
-            Collections.sort(locationsList,ALPHABETICAL_ORDER1);
+            locationsList10.add((String) locationsAdaptor10.getItem(i));
+            Collections.sort(locationsList10, ALPHABETICAL_ORDER1);
         }
 
         // -----------
 
         List<List<String>> allLocationLists = new ArrayList<List<String>>();
-        allLocationLists.add(locationsList);
+        allLocationLists.add(locationsList1);
         allLocationLists.add(locationsList2);
         allLocationLists.add(locationsList3);
         allLocationLists.add(locationsList4);
@@ -211,12 +157,24 @@ public class ExpandableListDataPump {
         allLocationLists.add(locationsList9);
         allLocationLists.add(locationsList10);
 
+        // ----------------
+
+        HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
+
+        String[] processesValues = context.getResources().getStringArray(R.array.processes);
+        Arrays.sort(processesValues);
+
+        ArrayAdapter<String> processesAdaptor = new ArrayAdapter<String>(context,
+                R.layout.places_list_item, R.id.placeNameTv, processesValues);
 
         for (int i = 0; i < processesAdaptor.getCount(); i++) {
+            String test = processesAdaptor.getItem(i).toString();
+            System.out.println(test);
             expandableListDetail.put(processesAdaptor.getItem(i), allLocationLists.get(i));
         }
 
-
         return expandableListDetail;
     }
+
+
 }
